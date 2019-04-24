a93bd6980e70442b75442b84e013367e4f55ab16
孙根华 <sungenhua@sungenhuadeMacBook-Pro.local>
2019-04-24 21:07:59 +0800

    update bubbleSort3

--- 十大经典排序算法/bubbleSort.js
+++ 十大经典排序算法/bubbleSort.js
@@ -4,8 +4,8 @@
  * @return {Array}
  */
 function bubbleSort(arr) {
-    let len = arr.length;
     console.time('传统冒泡排序耗时');
+    let len = arr.length;
     for(let i = 0; i < len; i++) {
         for(let j = 0; j < len - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
@@ -22,8 +22,9 @@ function bubbleSort(arr) {
 let arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 console.log(bubbleSort(arr))
 
+
 function bubbleSort2(arr) {
-    console.time('1.改进后冒泡排序耗时');
+    console.time('改良冒泡排序耗时');
     let i = arr.length - 1;
     while(i > 0) {
         let pos = 0;
@@ -32,23 +33,49 @@ function bubbleSort2(arr) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                // 记录交换位置
                 pos = j;
             }
         }
-        // 记录本次循环最后一次交换的位置
+        // 记录最后的交换位置
         i = pos;
     }
-    console.timeEnd('1.改进后冒泡排序耗时');
+    console.timeEnd('改良冒泡排序耗时')
     return arr;
 }
 
 let arr2 = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 console.log(bubbleSort2(arr2))
 
+
 function bubbleSort3(arr) {
-   
+    console.time('最终版冒泡排序耗时');
+    let low = 0,
+        high = arr.length - 1,
+        j,
+        temp;
+    while(low < high) {
+        for(j = low; j < high; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        low++;
+        for(j = high; j > low; j--) {
+            if(arr[j] < arr[ j - 1]) {
+                temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+        high--;
+    }
+    console.timeEnd('最终版冒泡排序耗时');
     return arr;
 }
 
 let arr3 = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 console.log(bubbleSort3(arr3))
+
